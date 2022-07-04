ab8d7b17be91b4a88600dd98de7683a19cf77c02
mesayusriana12 <mesayusriana12@gmail.com>
2022-07-04 19:36:27 +0700

    bugfix: revert default button type

--- resources/js/Jetstream/Button.vue
+++ resources/js/Jetstream/Button.vue
@@ -2,13 +2,13 @@
 defineProps({
     type: {
         type: String,
-        default: 'button',
+        default: 'submit',
     },
 });
 </script>
 
 <template>
-    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 hover:bg-gray-700 active:bg-gray-900 focus:bg-gray-900 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none  focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+    <button :type="type" class="inline-flex items-center px-4 py-2 bg-gray-800 hover:bg-gray-700 active:bg-gray-900 focus:bg-gray-900 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest focus:outline-none focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
         <slot />
     </button>
 </template>
