e8d0df4ea1582f75ac4d2bdaf4a01552d395d043
Geriano <gerznewbie@gmail.com>
2022-07-07 19:30:31 +0700

    create card component

---
+++ resources/js/Components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col border rounded-md">
+    <div class="bg-slate-100 rounded-t-md">
+      <slot name="header" />
+    </div>
+
+    <div class="p-4 bg-slate-50 border-y rounded-md">
+      <slot name="body" />
+    </div>
+
+    <div class="bg-slate-100 rounded-b-md">
+      <slot name="footer" />
+    </div>
+  </div>
+</template>
