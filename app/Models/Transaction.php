efffa34072061da3f7b61d2ed03d170997a3385c
mesayusriana12 <mesayusriana12@gmail.com>
2022-07-11 22:04:13 +0700

    add: create model and migration Transaction

---
+++ app/Models/Transaction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'user_id',
+        'total_cost',
+    ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function user()
+    {
+        return $this->hasOne(User::class, 'id', 'user_id');
+    }
+}
