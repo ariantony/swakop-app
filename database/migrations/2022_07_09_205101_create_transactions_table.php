cbe50efcc981ea643dd47c0d2592744e9fbe30b0
Geriano <gerznewbie@gmail.com>
2022-07-11 17:17:22 +0700

    create transaction migration

---
+++ database/migrations/2022_07_09_205101_create_transactions_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_id')->constrained()->cascadeOnDelete();
+            $table->enum('type', ['buy', 'sell']);
+            $table->unsignedInteger('qty_unit')->nullable()->default(null);
+            $table->unsignedInteger('qty_box')->nullable()->default(null);
+            $table->unsignedInteger('qty_carton')->nullable()->default(null);
+            $table->unsignedDouble('cost_unit', 20)->nullable()->default(null);
+            $table->unsignedDouble('cost_box', 20)->nullable()->default(null);
+            $table->unsignedDouble('cost_carton', 20)->nullable()->default(null);
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
