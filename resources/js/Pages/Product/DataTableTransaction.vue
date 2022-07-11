73abbce8006ff4e9ae15190ddc2ca0bc6ce12795
Geriano <gerznewbie@gmail.com>
2022-07-11 17:23:28 +0700

    create product transaction history data table

---
+++ resources/js/Pages/Product/DataTableTransaction.vue
@@ -0,0 +1,47 @@
+<script setup>
+import Builder from '@/Components/DataTable/Builder.vue'
+import Th from '@/Components/DataTable/Th.vue'
+
+const { product } = defineProps({
+  product: Object,
+})
+</script>
+
+<template>
+  <Builder :href="route('api.product.transactions.paginate', product.id)" :colspan="5">
+    <template v-slot:thead="{table}">
+      <tr>
+        <Th class="px-1 py-2 uppercase border-b-2 border-r-2 border-slate-300" :sortable="false">no</Th>
+        <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :table="table" name="user">user</Th>
+        <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :table="table" name="qty_unit">qty unit</Th>
+        <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :table="table" name="qty_box">qty box</Th>
+        <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :table="table" name="qty_carton">qty carton</Th>
+        <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :table="table" name="cost_unit">cost unit</Th>
+        <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :table="table" name="cost_box">cost box</Th>
+        <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :table="table" name="cost_carton">cost carton</Th>
+      </tr>
+    </template>
+    <template #tfoot>
+      <Th class="px-1 py-2 uppercase border-b-2 border-r-2 border-slate-300" :sortable="false">no</Th>
+      <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :sortable="false">user</Th>
+      <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :sortable="false">qty unit</Th>
+      <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :sortable="false">qty box</Th>
+      <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :sortable="false">qty carton</Th>
+      <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :sortable="false">cost unit</Th>
+      <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :sortable="false">cost box</Th>
+      <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :sortable="false">cost carton</Th>
+    </template>
+    <template v-slot:tbody="{ index, item }">
+      <tr>
+        <td class="border p-2 border-x-2 border-slate-300 text-center">{{ index + 1 }}</td>
+        <td class="border p-2 border-x-2 border-slate-300">{{ item.user.name }}</td>
+        <td class="border p-2 border-x-2 border-slate-300">{{ item.qty_unit }}</td>
+        <td class="border p-2 border-x-2 border-slate-300">{{ item.qty_box }}</td>
+        <td class="border p-2 border-x-2 border-slate-300">{{ item.qty_carton }}</td>
+        <td class="border p-2 border-x-2 border-slate-300">{{ item.cost_unit }}</td>
+        <td class="border p-2 border-x-2 border-slate-300">{{ item.cost_box }}</td>
+        <td class="border p-2 border-x-2 border-slate-300">{{ item.cost_carton }}</td>
+      </tr>
+    </template>
+  </Builder>
+</template>
---
+++ resources/js/Pages/Product/Transaction.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { getCurrentInstance, onMounted, onUnmounted, ref } from 'vue';
+import Builder from '@/Components/DataTable/Builder.vue'
+import Th from '@/Components/DataTable/Builder.vue'
+import DataTable from './DataTableTransaction.vue'
+
+const { product, close } = defineProps({
+  product: Object,
+  close: Function,
+})
+
+const hide = e => {
+  if (e.key === 'Escape') {
+    close()
+  
+    window.addEventListener('keyup', hide)
+  }
+}
+
+onMounted(() => window.addEventListener('keyup', hide))
+onUnmounted(() => window.removeEventListener('keyup', hide))
+</script>
+
+<template>
+  <transition name="slide-fade">
+    <div v-if="product !== null" class="fixed top-0 left-0 w-full h-screen flex items-center justify-center">
+      <div class="flex flex-col bg-slate-50 w-full max-w-5xl rounded-md">
+        <div class="flex items-center justify-between space-x-2 bg-slate-100 rounded-t-md py-2 px-4">
+          <div class="lowercase first-letter:capitalize text-2xl font-semibold">transaksi produk {{ product.name }}</div>
+
+          <button @click.prevent="close" type="button" class="rounded-md bg-red-500 text-white px-1">
+            <i class="bx bx-x text-xl"></i>
+          </button>
+        </div>
+
+        <div class="p-4 max-h-96 overflow-auto">
+          <DataTable :product="product" />
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
