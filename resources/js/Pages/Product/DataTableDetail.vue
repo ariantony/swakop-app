24d42d80bdc14bcfcfbeb25b61b9ead059a0ffce
mesayusriana12 <mesayusriana12@gmail.com>
2022-07-12 10:21:17 +0700

    styling: fix border on detail transaction

--- resources/js/Pages/Product/DataTableDetail.vue
+++ resources/js/Pages/Product/DataTableDetail.vue
@@ -11,25 +11,25 @@ const { product } = defineProps({
   <Builder :href="route('api.product.detail.paginate', product.id)" :colspan="8">
     <template v-slot:thead="{table}">
       <tr>
-        <Th class="px-1 py-2 uppercase border-b-2 border-r-2 border-slate-300" :sortable="false">no</Th>
-        <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :table="table" name="user">user</Th>
-        <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :table="table" name="qty_unit">qty unit</Th>
-        <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :table="table" name="qty_box">qty box</Th>
-        <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :table="table" name="qty_carton">qty carton</Th>
-        <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :table="table" name="cost_unit">cost unit</Th>
-        <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :table="table" name="cost_box">cost box</Th>
-        <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :table="table" name="cost_carton">cost carton</Th>
+        <Th class="px-1 py-2 uppercase border-2 border-slate-300" :sortable="false">no</Th>
+        <Th class="px-3 py-2 uppercase border-2 border-slate-300" :table="table" name="user">user</Th>
+        <Th class="px-3 py-2 uppercase border-2 border-slate-300" :table="table" name="qty_unit">qty unit</Th>
+        <Th class="px-3 py-2 uppercase border-2 border-slate-300" :table="table" name="qty_box">qty box</Th>
+        <Th class="px-3 py-2 uppercase border-2 border-slate-300" :table="table" name="qty_carton">qty carton</Th>
+        <Th class="px-3 py-2 uppercase border-2 border-slate-300" :table="table" name="cost_unit">cost unit</Th>
+        <Th class="px-3 py-2 uppercase border-2 border-slate-300" :table="table" name="cost_box">cost box</Th>
+        <Th class="px-3 py-2 uppercase border-2 border-slate-300" :table="table" name="cost_carton">cost carton</Th>
       </tr>
     </template>
     <template #tfoot>
-      <Th class="px-1 py-2 uppercase border-b-2 border-r-2 border-slate-300" :sortable="false">no</Th>
-      <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :sortable="false">user</Th>
-      <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :sortable="false">qty unit</Th>
-      <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :sortable="false">qty box</Th>
-      <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :sortable="false">qty carton</Th>
-      <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :sortable="false">cost unit</Th>
-      <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :sortable="false">cost box</Th>
-      <Th class="px-3 py-2 uppercase border-b-2 border-x-2 border-slate-300" :sortable="false">cost carton</Th>
+      <Th class="px-1 py-2 uppercase border-2 border-slate-300" :sortable="false">no</Th>
+      <Th class="px-3 py-2 uppercase border-2 border-slate-300" :sortable="false">user</Th>
+      <Th class="px-3 py-2 uppercase border-2 border-slate-300" :sortable="false">qty unit</Th>
+      <Th class="px-3 py-2 uppercase border-2 border-slate-300" :sortable="false">qty box</Th>
+      <Th class="px-3 py-2 uppercase border-2 border-slate-300" :sortable="false">qty carton</Th>
+      <Th class="px-3 py-2 uppercase border-2 border-slate-300" :sortable="false">cost unit</Th>
+      <Th class="px-3 py-2 uppercase border-2 border-slate-300" :sortable="false">cost box</Th>
+      <Th class="px-3 py-2 uppercase border-2 border-slate-300" :sortable="false">cost carton</Th>
     </template>
     <template v-slot:tbody="{ index, item }">
       <tr>
