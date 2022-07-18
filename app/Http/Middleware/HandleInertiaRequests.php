4366272ce13f64d6761bddd9f933aad0c48334a3
Geriano <gerznewbie@gmail.com>
2022-07-18 07:58:46 +0700

    create url mixin method

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,6 +39,7 @@ class HandleInertiaRequests extends Middleware
         return array_merge(parent::share($request), [
             'success' => session()->get('success'),
             'error' => session()->get('error'),
+            '$url' => url('/'),
         ]);
     }
 }
