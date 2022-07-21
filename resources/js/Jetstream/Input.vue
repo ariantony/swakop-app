3fb104a719a844d8438db66104c279a271266932
mesayusriana12 <mesayusriana12@gmail.com>
2022-07-21 19:15:29 +0700

    style: add autocomplete off in jet input

--- resources/js/Jetstream/Input.vue
+++ resources/js/Jetstream/Input.vue
@@ -20,7 +20,7 @@ defineExpose({ focus: () => input.value.focus() });
 
 <template>
     <input
-        ref="input"
+        ref="input" autocomplete="off"
         class="border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50 rounded-md shadow-sm"
         :value="modelValue"
         @input="$emit('update:modelValue', $event.target.value)"
