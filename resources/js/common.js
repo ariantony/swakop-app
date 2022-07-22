1d856502a296afbc1f610993714b18f90d7129ae
mesayusriana12 <mesayusriana12@gmail.com>
2022-07-22 12:54:56 +0700

    add: dateindo helper

--- resources/js/common.js
+++ resources/js/common.js
@@ -4,6 +4,20 @@ export const rupiah = value => new Intl.NumberFormat('id-ID', {
   minimumFractionDigits: 0,
 }).format(value);
 
+export const dateindo = value => {
+  const libMonth = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
+  
+  const date = new Date(value);
+  const getDay = date.getDate();
+  const getMonth = date.getMonth();
+  const getYear = date.getFullYear();
+
+  const month = libMonth[getMonth];
+  const year = (getYear < 1000) ? getYear + 1900 : getYear;
+  return `${getDay} ${month} ${year}`;
+}
+
 export default {
   rupiah,
+  dateindo,
 }
