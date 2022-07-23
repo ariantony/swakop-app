4fe77578e9e8df0ac1ae4e720184b5bf8d46b41d
Geriano <gerznewbie@gmail.com>
2022-07-23 10:46:30 +0700

    fix typo :'(

--- resources/js/Charts/SellingThisMonth.vue
+++ resources/js/Charts/SellingThisMonth.vue
@@ -165,8 +165,8 @@ onMounted(fetch)
 
 <template>
   <div class="bg-white rounded-md">
-    <h1 class="text-xl font-semibold px-4 pt-2">
-      Grafix penjualan bulan {{ id.localize.month(date.month) }} {{ date.year }}
+    <h1 class="text-3xl font-semibold px-4 pt-2">
+      Grafik penjualan bulan {{ id.localize.month(date.month) }} {{ date.year }}
     </h1>
     <VChart class="h-96 rounded-md" :option="option" />
   </div>
