7bcabc9adc6c5e4ad16ff78e99521baa61865eaa
mesayusriana12 <mesayusriana12@gmail.com>
2022-07-23 14:03:50 +0700

    add: argument in helper to show time

--- resources/js/common.js
+++ resources/js/common.js
@@ -4,7 +4,7 @@ export const rupiah = value => new Intl.NumberFormat('id-ID', {
   minimumFractionDigits: 0,
 }).format(value);
 
-export const dateindo = value => {
+export const dateindo = (value, withTime = false) => {
   const libMonth = ['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli', 'Agustus', 'September', 'Oktober', 'November', 'Desember'];
   
   const date = new Date(value);
@@ -14,7 +14,11 @@ export const dateindo = value => {
 
   const month = libMonth[getMonth];
   const year = (getYear < 1000) ? getYear + 1900 : getYear;
-  return `${getDay} ${month} ${year}`;
+  let result = `${getDay} ${month} ${year} `;
+
+  if (withTime) result += `${date.toLocaleTimeString('en-GB')}`
+  
+  return result;
 }
 
 export default {
