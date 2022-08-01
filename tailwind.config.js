2daf95ab3232eba2b5327ecaa791b95c3598f614
mesayusriana12 <mesayusriana12@gmail.com>
2022-08-01 20:35:04 +0700

    style: add 1 to 10 cm size

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,6 +17,18 @@ module.exports = {
             },
             colors: {
                 primary: '#dc1d22',
+            },
+            spacing: {
+                '1cm': '37.8px',
+                '2cm': '75.6px',
+                '3cm': '113.4px',
+                '4cm': '151.2px',
+                '5cm': '189.0px',
+                '6cm': '226.8px',
+                '7cm': '264.6px',
+                '8cm': '302.4px',
+                '9cm': '340.2px',
+                '10cm': '378.0px',
             }
         },
     },
