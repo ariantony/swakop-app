e9ac469115a367a4f3f407ece4a2714c40c1c0b6
mesayusriana12 <mesayusriana12@gmail.com>
2022-09-06 00:20:51 +0700

    style: group product print now fixed

--- resources/js/Pages/Product/Print/Group.vue
+++ resources/js/Pages/Product/Print/Group.vue
@@ -26,17 +26,19 @@ const print = () => {
       Swal.showLoading()
     }
   })
-  setTimeout(() => {
-    const iframe = document.createElement('iframe')
-    iframe.style.display = 'none'
-    iframe.src = route('product.iframe.group', {
-      group_id: group.id,
-    })
-    document.body.appendChild(iframe)
-    setTimeout(() => {
-      Swal.close()
-    }, 1000)
-  }, 1000)
+  
+  const iframe = document.createElement('iframe')
+  iframe.style.display = 'none'
+  iframe.src = route('product.iframe.group', {
+    group_id: group.id,
+  })
+  document.body.appendChild(iframe)
+
+  iframe.onload = () => {
+    Swal.close()
+  }
+  
+  setTimeout(() => { iframe.remove() }, 10000);
 }
 
 </script>
@@ -64,74 +66,29 @@ const print = () => {
         </div>
       </template>
       <template #body>
-        <table class="w-full border-collapse border-2 border-slate-300">
-          <thead class="bg-slate-100">
-            <tr>
-              <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" rowspan="2">No</th>
-              <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" rowspan="2">Kode</th>
-              <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" rowspan="2">Nama</th>
-              <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" rowspan="2">Barcode</th>
-              <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" colspan="1">Harga Jual</th>
-              <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" colspan="1">Stok</th>
-            </tr>
-            <tr>
-              <th class="px-3 py-2 uppercase border-2 border-slate-300">satuan</th>
-              <!-- <Th class="px-3 py-2 uppercase border-2 border-slate-300" :table="table" name="qty_box">box / renceng</Th>
-              <Th class="px-3 py-2 uppercase border-2 border-slate-300" :table="table" name="qty_carton">karton</Th> -->
-              <th class="px-3 py-2 uppercase border-2 border-slate-300">satuan</th>
-              <!-- <Th class="px-3 py-2 uppercase border-2 border-slate-300" :table="table" name="cost_box">box / renceng</Th>
-              <Th class="px-3 py-2 uppercase border-2 border-slate-300" :table="table" name="cost_carton">carton</Th> -->
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(item, i) in products" :key="i" :item="item">
-              <td class="border p-2 border-x-2 border-slate-300 text-center">{{ i + 1 }}</td>
-              <td class="border p-2 border-x-2 border-slate-300 capitalize">{{ item.code }}</td>
-              <td class="border p-2 border-x-2 border-slate-300 capitalize">{{ item.name }}</td>
-              <td class="border p-2 border-x-2 border-slate-300 capitalize">{{ item.barcode }}</td>
-              <td class="border p-2 border-x-2 border-slate-300 text-right">{{ rupiah(item.price.price_per_unit) }}</td>
-              <td class="border p-2 border-x-2 border-slate-300 text-center">{{ item.stock_unit }}</td>
-            </tr>
-          </tbody>
-        </table>
-      </template>
-
-      <template #print>
-        <div class="flex items-center justify-center font-semibold mb-10">
-          <h1 class="text-black text-2xl">Produk dengan Kelompok Barang "{{ group.code }}"</h1>
-        </div>
-        <table class="w-full border-2 border-slate-300">
-          <thead class="bg-slate-100">
-            <tr>
-              <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" rowspan="2">No</th>
-              <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" rowspan="2">Kode</th>
-              <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" rowspan="2">Nama</th>
-              <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" rowspan="2">Barcode</th>
-              <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" colspan="1">Harga Jual</th>
-              <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" colspan="1">Stok</th>
-            </tr>
-            <tr>
-              <th class="px-3 py-2 uppercase border-2 border-slate-300">satuan</th>
-              <!-- <Th class="px-3 py-2 uppercase border-2 border-slate-300" :table="table" name="qty_box">box / renceng</Th>
-              <Th class="px-3 py-2 uppercase border-2 border-slate-300" :table="table" name="qty_carton">karton</Th> -->
-              <th class="px-3 py-2 uppercase border-2 border-slate-300">satuan</th>
-              <!-- <Th class="px-3 py-2 uppercase border-2 border-slate-300" :table="table" name="cost_box">box / renceng</Th>
-              <Th class="px-3 py-2 uppercase border-2 border-slate-300" :table="table" name="cost_carton">carton</Th> -->
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="(item, i) in products" :key="i" :item="item">
-              <td class="border p-2 border-x-2 border-slate-300 text-center">{{ i + 1 }}</td>
-              <td class="border p-2 border-x-2 border-slate-300 capitalize">{{ item.code }}</td>
-              <td class="border p-2 border-x-2 border-slate-300 capitalize">{{ item.name }}</td>
-              <td class="border p-2 border-x-2 border-slate-300 capitalize">{{ item.barcode }}</td>
-              <td class="border p-2 border-x-2 border-slate-300 text-right">{{ rupiah(item.price.price_per_unit) }}</td>
-              <td class="border p-2 border-x-2 border-slate-300 text-center">{{ item.stock_unit }}</td>
-            </tr>
-          </tbody>
-        </table>
-        <div class="flex items-center justify-start text-black space-y-2 mt-2">
-          <div>File ini dicetak pada {{ dateindo(new Date(), true) }}</div>
+        <div class="h-[28rem] overflow-y-auto">
+          <table class="w-full border-collapse border-2 border-slate-300">
+            <thead class="bg-slate-100">
+              <tr class="bg-slate-100">
+                <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300">No</th>
+                <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300">Kode</th>
+                <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300">Nama</th>
+                <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300">Barcode</th>
+                <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300">Harga</th>
+                <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300 w-16">Stok</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="(item, i) in products" :key="i" :item="item">
+                <td class="border p-2 border-x-2 border-slate-300 text-center">{{ i + 1 }}</td>
+                <td class="border p-2 border-x-2 border-slate-300 capitalize">{{ item.code }}</td>
+                <td class="border p-2 border-x-2 border-slate-300 capitalize">{{ item.name }}</td>
+                <td class="border p-2 border-x-2 border-slate-300 capitalize">{{ item.barcode }}</td>
+                <td class="border p-2 border-x-2 border-slate-300 text-right">{{ rupiah(item?.price?.price_per_unit) }}</td>
+                <td class="border p-2 border-x-2 border-slate-300 text-center">{{ item.stock_unit }}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </template>
     </Card>
--- resources/js/Pages/Product/Print/IframeGroup.vue
+++ resources/js/Pages/Product/Print/IframeGroup.vue
@@ -20,16 +20,12 @@
     </div>
     <table class="w-full border-2 border-slate-300">
       <tr class="bg-slate-100">
-        <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" rowspan="2">No</th>
-        <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" rowspan="2">Kode</th>
-        <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" rowspan="2">Nama</th>
-        <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" rowspan="2">Barcode</th>
-        <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" colspan="1">Harga Jual</th>
-        <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300" colspan="1">Stok</th>
-      </tr>
-      <tr class="bg-slate-100">
-        <th class="px-3 py-2 uppercase border-2 border-slate-300">satuan</th>
-        <th class="px-3 py-2 uppercase border-2 border-slate-300">satuan</th>
+        <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300">No</th>
+        <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300">Kode</th>
+        <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300">Nama</th>
+        <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300">Barcode</th>
+        <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300">Harga</th>
+        <th class="px-1 py-2 uppercase border-b-2 border-x-2 border-slate-300 w-16">Stok</th>
       </tr>
       <tbody>
         <tr v-for="(item, i) in products" :key="i" :item="item">
@@ -37,7 +33,7 @@
           <td class="border p-2 border-x-2 border-slate-300 capitalize">{{ item.code }}</td>
           <td class="border p-2 border-x-2 border-slate-300 capitalize">{{ item.name }}</td>
           <td class="border p-2 border-x-2 border-slate-300 capitalize">{{ item.barcode }}</td>
-          <td class="border p-2 border-x-2 border-slate-300 text-right">{{ rupiah(item.price.price_per_unit) }}</td>
+          <td class="border p-2 border-x-2 border-slate-300 text-right">{{ item.price?.price_per_unit ? rupiah(item.price?.price_per_unit) : '-' }}</td>
           <td class="border p-2 border-x-2 border-slate-300 text-center">{{ item.stock_unit }}</td>
         </tr>
       </tbody>
